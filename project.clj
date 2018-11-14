1f988550f5ca976911703e50e67f9c11e1b3762f
Gianluca Scacco <gianluca.scacco@gmail.com>
2018-11-14 15:25:56 +0100

    Gscacco fork

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.4.0"
+(defproject org.clojars.gscacco/lein-gorilla "0.5.0"
   :description "A Leiningen plugin for the Gorilla REPL."
-  :url "https://github.com/JonyEpsilon/lein-gorilla"
+  :url "https://github.com/gscacco/lein-gorilla.git"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.4.0"]]
+  :dependencies [[org.clojars.gscacco/gorilla-repl "0.5.0"]]
   :eval-in-leiningen true)
