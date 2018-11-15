d6dacf6a460025f83241a600966d28b958d5d396
Gianluca Scacco <gianluca.scacco@gmail.com>
2018-11-15 09:40:01 +0100

    Now using gorilla-repl 0.5.2

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/gscacco/lein-gorilla.git"
   :license {:name "MIT"}
-  :dependencies [[org.clojars.gscacco/gorilla-repl "0.5.1"]]
+  :dependencies [[org.clojars.gscacco/gorilla-repl "0.5.2"]]
   :eval-in-leiningen true)
